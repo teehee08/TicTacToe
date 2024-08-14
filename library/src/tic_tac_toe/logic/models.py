2c1405e7e78ee163d1296bbe40fd3ada9ddc3f71
Wen Hui TEE <wenhuiteesi@astri.org>
2024-08-14 16:08:33 +0800

    edit models.py

---
+++ library/src/tic_tac_toe/logic/models.py
@@ -0,0 +1,10 @@
+import enum
+from __future__ import annotations
+
+class Mark(enum.StrEnum):
+    CROSS = "X"
+    NAUGHT = "O"
+
+    @property
+    def other(self) -> Mark:
+        return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
