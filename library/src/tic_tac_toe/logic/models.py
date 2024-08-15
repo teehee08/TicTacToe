95c57819307d9f1f43eca91fb3f7eca599852a8f
Wen Hui TEE <wenhuiteesi@astri.org>
2024-08-15 14:52:01 +0800

    added some data class

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -1,5 +1,6 @@
 import enum
 from __future__ import annotations
+import re
 
 class Mark(enum.StrEnum):
     CROSS = "X"
@@ -8,3 +9,39 @@ class Mark(enum.StrEnum):
     @property
     def other(self) -> Mark:
         return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
+
+from dataclasses import dataclass
+from functools import cached_property
+@dataclass(frozen=True)
+class Grid:
+    cells: str = " " * 9
+
+    def __post_init__(self) -> None:
+        if not re.match(r"^[\sXO]{9}$", self.cells):
+            raise ValueError("Must contain 9 cells of: X, O, or space")
+        
+    @cached_property
+    def x_count(self) -> int:
+        return self.cells.count("X")
+    
+    @cached_property
+    def O_count(self) -> int:
+        return self.cells.count("O")        
+
+    @cached_property
+    def empty_count(self) -> int:
+        return self.cells.count(" ")
+    
+@dataclass(frozen=True)
+class Move:
+    mark: Mark
+    cell_index: int
+    before_state: "GameState"
+    after_state: "GameState"
+
+@dataclass(frozen=True)
+class GameState:
+    grid: Grid
+    starting_mark: Mark = Mark ("X")
+
+    
